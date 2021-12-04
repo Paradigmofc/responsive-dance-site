dd273339567e73ea8736ba43b343d10da782797f
Alexey <paradigmofc@gmail.com>
2021-12-04 18:12:34 +0200

    button up

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function($) {
+	"use strict"
+
+    // button up
+
+	  var btn = $('#button');  
+
+	  $(window).scroll(function() {     
+	    if ($(window).scrollTop() > 300) {
+	       btn.addClass('show');
+	     } else {
+	       btn.removeClass('show');
+	     }
+	   });
+
+	   btn.on('click', function(e) {
+	     e.preventDefault();
+	     $('html, body').animate({scrollTop:0}, 100);
+	   });
+})(jQuery);
