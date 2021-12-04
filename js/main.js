ec775f32abbd5b577283633061d3c35ad008aadb
Alexey <paradigmofc@gmail.com>
2021-12-04 18:44:52 +0200

    Button up back

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
-// (function($) {
-// 	"use strict"
+var btn = $('#button');
 
-//     // button up
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
 
-// 	  var btn = $('.arrowUp');  
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '100');
+});
 
-// 	  $(window).scroll(function() {     
-// 	    if ($(window).scrollTop() > 300) {
-// 	       btn.addClass('show');
-// 	     } else {
-// 	       btn.removeClass('show');
-// 	     }
-// 	   });
-
-// 	   btn.on('click', function(e) {
-// 	     e.preventDefault();
-// 	     $('html, body').animate({scrollTop:0}, 100);
-// 	   });
-// })(jQuery);
